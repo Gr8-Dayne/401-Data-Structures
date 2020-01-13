94dab47562b5066a4289f5eec1075ac8869e1a13
Dayne Daylong <dalexad@gmail.com>
2020-01-13 13:15:19 -0800

    progress with toString test

--- challenges/src/test/java/linkedlist/LinkedListTest.java
+++ challenges/src/test/java/linkedlist/LinkedListTest.java
@@ -29,17 +29,6 @@ public class LinkedListTest {
 
     }
 
-//    Can properly insert multiple nodes into the linked list
-    @Test public void verifyMultipleNodes() {
-
-        LinkedList list = new LinkedList();
-        list.insertHead(20);
-        list.insertHead(10);
-        list.insertHead(50);
-        assertEquals(50, list.head.valueData);
-
-    }
-
 //    Will return true when finding a value within the linked list that exists
 //    Will return false when searching for a value in the linked list that does not exist
     @Test public void booleanValueSearch() {
@@ -52,8 +41,16 @@ public class LinkedListTest {
 
     }
 
+//    Can properly insert multiple nodes into the linked list
 //    Can properly return a collection of all the values that exist in the linked list
     @Test public String toString() {
+
+        LinkedList list = new LinkedList();
+        list.insertHead(20);
+        list.insertHead(10);
+        list.insertHead(50);
+
+        expectedAnswer = "{ 20 } => { 10 } => { 50 } => null";
 
         return overallStatement + null;
     }
