dac83a22331046aef89085090f66dc515e5bbef8
Dayne Daylong <dalexad@gmail.com>
2020-01-21 10:14:58 -0800

    good start

--- challenges/src/main/java/stacksandqueues/PseudoQueue.java
+++ challenges/src/main/java/stacksandqueues/PseudoQueue.java
@@ -1,12 +1,65 @@
 package stacksandqueues;
 
+
 public class PseudoQueue {
 
+    Node front;
+    Node back;
 
+    public void push(int numberVal) {
+        front = new Node(numberVal, front);
+    }
 
-    public PseudoQueue() {
+    public void pseudoQueueEnqueue(int pseudoVal) {
+        if (front == null){
+        push(pseudoVal);
+        } else {
+            back.next = new Node(pseudoVal, null);
+            back = back.next;
+        }
+    }
 
+//    public void enqueue(int numberVal) {
+//        if (front == null){
+//            front = new Node(numberVal, null);
+//            back = front;
+//        } else {
+//            back.next = new Node(numberVal, null);
+//            back = back.next;
+//        }
+//    }
 
+    public int pop() {
+        int popVal = front.valueData;
+        front = front.next;
+        return popVal;
+    }
+
+    public int pseudoQueueDequeue() {
+
+        int popVal = front.valueData;
+        front = front.next;
+        return popVal;
+
+//        int dequeueVal = 0;
+//        if(front != null){
+//            dequeueVal = front.valueData;
+//            front = front.next;
+//        }
+//        return dequeueVal;
+    }
+
+//    public int dequeue() {
+//        int dequeueVal = 0;
+//        if(front != null){
+//            dequeueVal = front.valueData;
+//            front = front.next;
+//        }
+//        return dequeueVal;
+//    }
+
+    public void display(){
+        System.out.println(front.toString());
     }
 
 }
--- challenges/src/test/java/stacksandqueues/PseudoQueueTest.java
+++ challenges/src/test/java/stacksandqueues/PseudoQueueTest.java
@@ -1,7 +1,34 @@
 package stacksandqueues;
 
+
+import org.junit.Test;
 import static org.junit.Assert.*;
+
 
 public class PseudoQueueTest {
 
+    PseudoQueue pseudoForTesting;
+
+    @Test public void pseudoQueueEnqueueTest() {
+        pseudoForTesting = new PseudoQueue();
+        pseudoForTesting.pseudoQueueEnqueue(1);
+        pseudoForTesting.display();
+        pseudoForTesting.pseudoQueueEnqueue(2);
+        pseudoForTesting.display();
+        pseudoForTesting.pseudoQueueEnqueue(3);
+        pseudoForTesting.display();
+        int actual = pseudoForTesting.front.valueData;
+        assertEquals(1, actual);
+    }
+
+    @Test public void pseudoQueueDequeueTest() {
+        pseudoForTesting = new PseudoQueue();
+        pseudoForTesting.pseudoQueueEnqueue(1);
+        pseudoForTesting.pseudoQueueEnqueue(2);
+        pseudoForTesting.pseudoQueueEnqueue(3);
+        pseudoForTesting.display();
+        int actual = pseudoForTesting.pseudoQueueDequeue();
+        pseudoForTesting.display();
+        assertEquals(1, actual);
+    }
 }
