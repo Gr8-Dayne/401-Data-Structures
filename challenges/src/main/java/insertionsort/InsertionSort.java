3227f3143e3212f2001b49faabbfb4363d8b612c
Dayne Daylong <dalexad@gmail.com>
2020-02-11 14:25:01 -0800

    created scaffold for CC26

---
+++ challenges/src/main/java/insertionsort/InsertionSort.java
@@ -0,0 +1,25 @@
+package insertionsort;
+
+
+public class InsertionSort{
+
+    public static void insertionSort(int[] arr) {
+
+
+
+    }
+}
+
+
+//    InsertionSort(int[] arr)
+//
+//    FOR i = 1 to arr.length
+//
+//        int j <-- i - 1
+//        int temp <-- arr[i]
+//
+//        WHILE j >= 0 AND temp < arr[j]
+//        arr[j + 1] <-- arr[j]
+//        j <-- j - 1
+//
+//        arr[j + 1] <-- temp
---
+++ challenges/src/test/java/insertionsort/InsertionSortTest.java
@@ -0,0 +1,16 @@
+package insertionsort;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class InsertionSortTest {
+
+    @Test
+    public void insertionSort() {
+
+
+
+    }
+}
