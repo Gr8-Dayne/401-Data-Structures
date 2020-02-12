e13f4c4b394e8c7dc5892f0d1e167f1762881e38
Dayne Daylong <dalexad@gmail.com>
2020-02-12 15:28:38 -0800

    initial MergeSort creation

--- challenges/src/main/java/packageofsorts/OfSorts.java
+++ challenges/src/main/java/packageofsorts/OfSorts.java
@@ -42,23 +42,55 @@ public class OfSorts {
 
     public static int[] mergeSort(int[] arr) {
 
-        if (arr.length == 0){
-            throw new IllegalArgumentException("Empty Array Detected - Operation Aborted");
-        }
+        // MergeSort
+//        ALGORITHM Mergesort(arr)
+//                DECLARE n <-- arr.length
 
-//        int arrIndexLength = arr.length;
-//
-//        for (int i = 1; i < arrIndexLength; ++i) {
-//
-//            int temp = arr[i];
-//            int j = i - 1;
-//
-//            while (j >= 0 && arr[j] > temp) {
-//                arr[j + 1] = arr[j];
-//                j = j - 1;
-//            }
-//            arr[j + 1] = temp;
-//        }
+//        if n > 1
+//          DECLARE mid <-- n/2
+//          DECLARE left <-- arr[0...mid]
+//          DECLARE right <-- arr[mid...n]
+//          // sort the left side
+//          Mergesort(left)
+//          // sort the right side
+//          Mergesort(right)
+//          // merge the sorted left and right sides together
+//          Merge(left, right, arr)
+
+        int arrIndexLength = arr.length;
+
+        if (arrIndexLength == 0){
+            throw new IllegalArgumentException("Empty Array Detected - Operation Aborted");
+
+        } else if (arrIndexLength > 1){
+
+            int mid = arrIndexLength/2;
+//            int left = arr[0 to mid];
+//            int right = arr[mid to arrIndexLength];
+        }
         return arr;
     }
+
+        // Merge
+//        ALGORITHM Merge(left, right, arr)
+//          DECLARE i <-- 0
+//          DECLARE j <-- 0
+//          DECLARE k <-- 0
+
+//        while i < left.length && j < right.length
+//          if left[i] <= right[j]
+//              arr[k] <-- left[i]
+//              i <-- i + 1
+//          else
+//              arr[k] <-- right[j]
+//              j <-- j + 1
+
+//          k <-- k + 1
+
+//        if i = left.length
+//            set remaining entries in arr to remaining values in right
+//        else
+//            set remaining entries in arr to remaining values in left
 }
+
+
--- challenges/src/test/java/packageofsorts/OfSortsTest.java
+++ challenges/src/test/java/packageofsorts/OfSortsTest.java
@@ -2,9 +2,9 @@ package packageofsorts;
 
 
 import org.junit.Test;
-import static insertionsort.InsertionSort.insertionSort;
-import static insertionsort.InsertionSort.printArray;
 import static org.junit.Assert.*;
+import static packageofsorts.OfSorts.insertionSort;
+import static packageofsorts.OfSorts.printArray;
 
 
 public class OfSortsTest {
@@ -43,12 +43,9 @@ public class OfSortsTest {
 
         inputArr = new int[]{8, 4, 23, 42, 16, 15};
 
-        // System.out.println("inputArray = " + printArray(inputArr));
         int[] answer = insertionSort(inputArr);
         String expected = "[4, 8, 15, 16, 23, 42]";
         String actual = printArray(answer);
-        // System.out.println("expected = " + expected);
-        // System.out.println("actual = " + actual);
         assertEquals(expected, actual);
     }
 
@@ -57,12 +54,9 @@ public class OfSortsTest {
 
         inputArr = new int[]{20, 18, 12, 8, 5, -2};
 
-        // System.out.println("inputArray = " + printArray(inputArr));
         int[] answer = insertionSort(inputArr);
         String expected = "[-2, 5, 8, 12, 18, 20]";
         String actual = printArray(answer);
-        // System.out.println("expected = " + expected);
-        // System.out.println("actual = " + actual);
         assertEquals(expected, actual);
     }
 
@@ -71,12 +65,9 @@ public class OfSortsTest {
 
         inputArr = new int[]{5, 12, 7, 5, 5, 7};
 
-        // System.out.println("inputArray = " + printArray(inputArr));
         int[] answer = insertionSort(inputArr);
         String expected = "[5, 5, 5, 7, 7, 12]";
         String actual = printArray(answer);
-        // System.out.println("expected = " + expected);
-        // System.out.println("actual = " + actual);
         assertEquals(expected, actual);
     }
 
@@ -85,12 +76,9 @@ public class OfSortsTest {
 
         inputArr = new int[]{2, 3, 5, 7, 13, 11};
 
-        // System.out.println("inputArray = " + printArray(inputArr));
         int[] answer = insertionSort(inputArr);
         String expected = "[2, 3, 5, 7, 11, 13]";
         String actual = printArray(answer);
-        // System.out.println("expected = " + expected);
-        // System.out.println("actual = " + actual);
         assertEquals(expected, actual);
     }
 
@@ -98,5 +86,31 @@ public class OfSortsTest {
     // 02/12/2020
     //
 
+    @Test
+    public void mergeSortTest1() {
 
+        inputArr = new int[]{8, 4, 23, 42, 16, 15};
+
+    }
+
+    @Test
+    public void mergeSortTest2() {
+
+        inputArr = new int[]{20, 18, 12, 8, 5, -2};
+
+    }
+
+    @Test
+    public void mergeSortTest3() {
+
+        inputArr = new int[]{5, 12, 7, 5, 5, 7};
+
+    }
+
+    @Test
+    public void mergeSortTest4() {
+
+        inputArr = new int[]{2, 3, 5, 7, 13, 11};
+
+    }
 }
