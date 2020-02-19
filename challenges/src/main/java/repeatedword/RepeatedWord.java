d87983019c7e146976fb1af127231e9bb44cd762
Dayne Daylong <dalexad@gmail.com>
2020-02-19 12:40:35 -0800

    added WB and outlined clear objectives for coding

---
+++ challenges/src/main/java/repeatedword/RepeatedWord.java
@@ -0,0 +1,25 @@
+package repeatedword;
+
+public class RepeatedWord {
+
+    public static String firstRepeated(String input) {
+
+        // Iterate through input
+
+        // Save each word as it's own String (separate/split by spaces: " ")
+        // Save into [something?]
+
+        // If wordToBeSaved == alreadySavedWord from alreadySavedWords
+        // Return this.wordToBeSaved
+
+        // Else
+        // Return: "No matching words from inputted String"
+
+        // Else
+        // Return: "ERROR incorrect data type for this method"
+
+        return null;
+    }
+}
+
+
---
+++ challenges/src/test/java/repeatedword/RepeatedWordTest.java
@@ -0,0 +1,27 @@
+package repeatedword;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class RepeatedWordTest {
+
+    // Test for Happy Case
+    // Test for Edge Cases
+    // Test for Expected Failure
+
+    @Test
+    public void firstRepeated() {
+    }
+
+    // Test for Empty String
+
+    // Test for No Matches
+
+    // Test for Uppercase/Lowercase
+
+    // Test for Incorrect Data Type Input
+}
+
+
